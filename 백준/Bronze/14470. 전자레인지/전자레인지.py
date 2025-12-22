b69266ae82570cf64181d823bf41160c29f36db4
수정 <peachily1108@gmail.com>
2025-12-22 18:52:54 +0900

    [Bronze IV] Title: 전자레인지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.py
@@ -0,0 +1,15 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+
+time = 0
+
+if A < 0:
+    time = ((0-A) * C) + D + (B * E)
+
+else:
+    time = (B-A) * E
+
+print(time)
